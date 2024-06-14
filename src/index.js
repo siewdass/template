82311159380f727f34c2f0cf478c7b5ead244094
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-14 12:32:58 -0400

    x

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import { createBrowserRouter, RouterProvider, useNavigate } from "react-router-d
 
 window.React = React
 
+const useFetch = async ( method, url, data ) => {
+  const response = await fetch( method == 'GET' ? `${ url }?${ new URLSearchParams( data ) }` : url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: method !== 'GET' ? JSON.stringify( data ) : null
+  } )
+  return await response.json( )
+}
+
 const router = createBrowserRouter( Object.keys( exports.config ).map( path => {
   const Component = exports[ exports.config[ path ] ]
-  return { path: path == '/' ? '*' : path , element: <Component { ...{ useEffect, useState, useNavigate } } /> }
+  return { path: path == '/' ? '*' : path , element: <Component { ...{ useEffect, useState, useNavigate, useFetch } } /> }
 } )  )
 
 const root = createRoot( document.getElementById( "root") )
